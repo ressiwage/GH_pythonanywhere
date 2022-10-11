08e3856974f0ffc8c798d92c027e6dce972623b2
1 <ressivage@ya.ru>
2022-10-11 17:41:45 +0500

    1234

--- post_on_PAW.py
+++ post_on_PAW.py
@@ -1,1 +1,26 @@
-print(1)
+import requests
+from os import walk 
+from os.path import abspath, dirname
+
+username = 'pavelmalevin'
+token = '50872cbedb63ef8730058f34022f10fcb3e4e940'
+path_to_file='/home/pavelmalevin/GH_paw_test'
+paw_domen='https://www.pythonanywhere.com'
+
+def post_file(filename, filecontent):
+    response = requests.post(
+        paw_domen+rf'/api/v0/user/{username}/files/path{path_to_file}'+rf'/{filename}',
+        headers={'Authorization': f'Token {token}'},
+        files={'content': rf'{filecontent}'}
+    )
+    print(response.status_code, response.content, sep="\n\n")
+
+def get_files_in_dir():
+    mypath = dirname(abspath(__file__))
+    filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+    return filenames
+
+for i in get_files_in_dir():
+    with open(i) as f:
+        s = f.read()
+    post_file(i, s)
